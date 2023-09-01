cdd74952e7e0e7e22b5f7db9cb3c380bc36ccc8c
MichalSmyk <michal.smyk38@gmail.com>
2023-09-01 15:45:12 +0100

    add test for album

---
+++ 01_music_library/tests/test_album.py
@@ -0,0 +1,11 @@
+from lib.album import Album
+"""
+Constructs with id, title, release_year and artist_id
+"""
+
+def test_construct_with_fields():
+    album = Album(id, "Dark Side", 1995, 2)
+    assert album.id == 1
+    assert album.title == "Dark Side"
+    assert album.release_year == 1995 
+    assert album.artist_id == 2
