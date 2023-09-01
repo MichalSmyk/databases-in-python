afe82a9a64e0c61bc362b76d1b1ab6373c64c8d0
MichalSmyk <michal.smyk38@gmail.com>
2023-09-01 15:47:39 +0100

    add class

--- 01_music_library/tests/test_album.py
+++ 01_music_library/tests/test_album.py
@@ -4,7 +4,7 @@ Constructs with id, title, release_year and artist_id
 """
 
 def test_construct_with_fields():
-    album = Album(id, "Dark Side", 1995, 2)
+    album = Album(1, "Dark Side", 1995, 2)
     assert album.id == 1
     assert album.title == "Dark Side"
     assert album.release_year == 1995 
